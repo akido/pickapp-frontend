bff1819b8b78a0a41a6d41ad2a3b8452e0a6095f
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2020-04-04 18:55:45 +0200

    LoginPage styles

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,34 @@
 import React, { useState } from 'react'
+import styled from '@emotion/styled'
+
+const Page = styled.div`
+  padding: 16px;
+  max-width: 480px;
+  margin: 0 auto;
+  .form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    input {
+      height: 52px;
+      border: 2px solid black;
+      /* border-radius: 4px; */
+      padding: 16px;
+      box-sizing: border-box;
+      margin-bottom: 16px;
+      font-size: 16px;
+    }
+    button {
+      height: 52px;
+      font-size: 16px;
+      background-color: black;
+      border: none;
+      color: white;
+      font-weight: bold;
+      text-transform: uppercase;
+    }
+  }
+`
 
 const LoginPage = (): React.FunctionComponentElement<{}> => {
   const [email, setEmail] = useState<string>('')
@@ -10,31 +40,32 @@ const LoginPage = (): React.FunctionComponentElement<{}> => {
   }
 
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor="email">Email</label>
-      <input
-        id="email"
-        name="email"
-        type="text"
-        value={email}
-        onChange={(event) => {
-          setEmail(event.target.value)
-        }}
-      />
-
-      <label htmlFor="password">Password</label>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        value={password}
-        onChange={(event) => {
-          setPassword(event.target.value)
-        }}
-      />
-
-      <button type="submit">Log in</button>
-    </form>
+    <Page>
+      <h1>Sign in</h1>
+      <form className="form" onSubmit={onSubmit}>
+        <input
+          id="email"
+          name="email"
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value)
+          }}
+        />
+        <input
+          id="password"
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value)
+          }}
+        />
+        <button type="submit">Log in</button>
+      </form>
+    </Page>
   )
 }
 
