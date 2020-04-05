8007d219bb317cba55102e104221e47b75bfab8f
Denis Shtabnoy <denis.shtabnoy@gmail.com>
2020-04-05 02:24:11 +0200

    Add logout button in Profile

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,15 +1,24 @@
 /** @jsx jsx */
-import React from 'react'
+import React, { useContext } from 'react'
 import Page from '../components/Page'
 import { jsx } from '@emotion/core'
+import { AuthContext } from '../contexts/Auth'
 
 const ProfilePage: React.FunctionComponent = () => {
+  const auth = useContext(AuthContext)
   const user = JSON.parse(localStorage.getItem('user'))
 
   return (
     <Page>
       <h1>Profile</h1>
       <div>Email: {user.email}</div>
+      <button
+        onClick={() => {
+          auth.logout()
+        }}
+      >
+        Log out
+      </button>
     </Page>
   )
 }
